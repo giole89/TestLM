70d5a2c73aa11ae62f94a72d4654f749b245a042
giole89 <giorgiolecce@gmail.com>
2017-10-23 15:31:31 +0200

    fix

--- src/main/java/org/gio/lmTest/utiity/Receipt.java
+++ src/main/java/org/gio/lmTest/utiity/Receipt.java
@@ -2,13 +2,10 @@ package org.gio.lmTest.utiity;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.gio.lmTest.bean.Cart;
-import org.gio.lmTest.bean.Product;
 import org.gio.lmTest.bean.QuantityProduct;
-import org.gio.lmTest.enumeration.Tax;
 
 public class Receipt {
 
@@ -43,25 +40,5 @@ public class Receipt {
 			total += taxedPrice * quantityProduct;
 		}
 		return new BigDecimal(total).setScale(2, RoundingMode.HALF_EVEN);
-	}
-
-	public static void main(String[] args) {
-		Product product = new Product("Libro", 14.99, Tax.TAX_5);
-		Product productDUE = new Product("CD", 100.00, Tax.TAX_10);
-
-		QuantityProduct quantity = new QuantityProduct(1, product);
-		QuantityProduct quantityDue = new QuantityProduct(1, productDUE);
-		List<QuantityProduct> quantityList = new ArrayList<QuantityProduct>();
-		quantityList.add(quantity);
-		quantityList.add(quantityDue);
-		Cart cart = new Cart(quantityList);
-		Receipt receipt = new Receipt(cart);
-
-		for (QuantityProduct quantityProduct : quantityList) {
-			System.out.println(quantityProduct.getQuantity() + " " + quantityProduct.getProduct().getName() + " "
-					+ quantityProduct.getProduct().taxedPrice());
-		}
-		System.out.println("Sales Taxes: " + receipt.taxSell());
-		System.out.println("TOTAL: " + receipt.getTotalPrice());
 	}
 }
